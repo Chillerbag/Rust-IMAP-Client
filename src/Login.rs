188dfb4d76c9a7a199a4144a4ea1b5499cce4da5
Ethan Hawkins <ethanwhawkins@gmail.com>
2024-05-02 22:30:51 +1000

    fixed the initial login, read_to_string says it goes to EOF, turns out, that does not equal \r\n, so, we need to make a reader

--- src/Login.rs
+++ src/Login.rs
@@ -1,59 +1,86 @@
 use std::net::{TcpStream};
 use std::io::{Read, Write};
 use std::process;
+use std::io::{BufRead, BufReader};
 
 
 // TODO probably want to return result here and deal with possible errors
+// actually, spec says to do this in mean
+// TODO put in general stream writing and reading behaviour in a seperate function
 
-// Function to send a command to the IMAP server
+
 pub fn login(stream: &mut TcpStream, command_id: &mut String, username: &str, password: &str, folder: &str, command_number: &mut u32){
     let mut response = String::new();
 
-    // logging in 
-    let full_command = format!("{} LOGIN {} {} \r\n", &command_id, &username, &password);
+    // -------------------------------- logging in --------------------------------
 
-    // testing
-    println!("{}", full_command);
-    match stream.write(full_command.as_bytes()) {
-        Ok(_) => {},
+    let full_command = format!("{} LOGIN {} {} \r\n", &command_id, &username, &password);
+    println!("command being writen: {}", full_command);
+
+    // Write command to server
+    match stream.write_all(full_command.as_bytes()) {
+        Ok(_) => println!("Successfully written login command"),
         Err(err) => {
             eprintln!("Error writing to stream: {}", err);
             process::exit(1);
         }
     }
-    // prints up to here:
-    println!("successfully written");
 
-    // hangs here:
-    if let Err(_) = stream.read_to_string(&mut response) {
-        println!("Login failure\n");
-        process::exit(3);
+    // clear response or it cries idk
+    response.clear();
+
+    // Read server response until end of line
+    let mut reader = BufReader::new(stream.try_clone().expect("error cloning stream"));
+    match reader.read_line(&mut response) {
+        Ok(_) => println!("Server response to login: {}", response),
+        Err(err) => {
+            eprintln!("Error reading from stream: {}", err);
+            process::exit(1);
+        }
     }
 
-    println!("successfully read to response");
-    //stream.read_to_string(&mut response);
-    println!("Response: {}", response);
+    response.clear();
 
     // command of logging is executed, so increment
     *command_number += 1;
     *command_id = format!("A{}", *command_number);
+    println!("{}", command_id);
 
-    // selecting the folder
+    // ---------------------------------------------------------------------------
 
-    // todo: if no folder is provided, read from inbox
-    let full_command = format!("{} SELECT {} \r\n", &command_id, folder);
-    response.clear();
+
+    
+
+    // ------------------------- selecting the folder ----------------------------
+
+    // TODO: if no folder is provided, read from inbox
+
+    let full_command = format!("\n{} PLUNKO {} \r\n", command_id, folder);
+    println!("command being written: {}", full_command);
+
     match stream.write_all(full_command.as_bytes()) {
-        Ok(_) => {},
+        Ok(_) => println!("Successfully written select folder command"),
         Err(err) => {
             eprintln!("Error writing to stream: {}", err);
             process::exit(1);
         }
     }
-    //stream.read_to_string(&mut response);
-    println!("Response: {}", response);
+
+    //response.clear();
+
+    // Read server response until end of line
+    //let mut reader = BufReader::new(stream.try_clone().expect("error cloning stream"));
+    match reader.read_line(&mut response) {
+        Ok(_) => println!("Server response to folder selection: {}", response),
+        Err(err) => {
+            eprintln!("Error reading from stream: {}", err);
+            process::exit(1);
+        }
+    }
 
     *command_number += 1;
     *command_id = format!("A{}", *command_number);
+
+    // ---------------------------------------------------------------------------
 
 }
