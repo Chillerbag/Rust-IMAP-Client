b9a340b272a7369f9414dc82e20c2b510cb79a31
Ethan Hawkins <ethanwhawkins@gmail.com>
2024-05-14 22:03:58 +1000

    accidently printing to eprintln

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -3,6 +3,5 @@ pub mod list;
 pub mod mime;
 pub mod parse;
 pub mod retrieve;
-
 use crate::helpers;
 use helpers::send_and_receive;
--- src/commands/parse.rs
+++ src/commands/parse.rs
@@ -62,18 +62,18 @@ pub fn parse_command(stream: &mut TcpStream, message_num: &mut String, command_n
 
     eprintln!("{:?}", env_from.address);
     if formatted_mailbox_name != "" {
-        eprintln!("From: {:?} {:?}",formatted_mailbox_name, formatted_mailbox);
+        println!("From: {:?} {:?}",formatted_mailbox_name, formatted_mailbox);
     }
     else {
-        eprintln!("From: {:?}",formatted_mailbox);
+        println!("From: {:?}",formatted_mailbox);
     }
-    eprintln!("To: {:?}",formatted_mailbox_to);
-    eprintln!("Date: {:?}",env_date.as_ref().unwrap_or(&"NIL".to_string()));
+    println!("To: {:?}",formatted_mailbox_to);
+    println!("Date: {:?}",env_date.as_ref().unwrap_or(&"NIL".to_string()));
     if env_subject.as_ref().unwrap_or(&"NIL".to_string()) != "NIL" {
-        eprintln!("Subject: {:?}",env_subject.as_ref().unwrap());
+        println!("Subject: {:?}",env_subject.as_ref().unwrap());
     }
     else {
-        eprintln!("Subject: <No subject>")
+        println!("Subject: <No subject>")
     }
 
 }
